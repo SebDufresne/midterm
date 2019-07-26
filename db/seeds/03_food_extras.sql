7518a61943069cabd8582d9e09fccea303799b4a
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-26 21:33:22 +0000

    Added seeds

---
+++ db/seeds/03_food_extras.sql
@@ -0,0 +1,9 @@
+INSERT INTO food_extras (name, price)
+VALUES
+('mushroom',50),
+('hot peppers',75),
+('pickled cucumber  ',150),
+('sauerkraut',150),
+('mustard',50),
+('ketchup',100),
+('anchovy',150);
