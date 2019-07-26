7b3a3acc140011ab56959fc08014f6cfc064bbd9
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-26 20:20:43 +0000

    Added seeds for users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,18 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name) VALUES ('Alice');
 INSERT INTO users (name) VALUES ('Kira');
+
+INSERT INTO users (name,email,phone_number,password, admin)
+VALUES ('admin','admin@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', TRUE);
+
+INSERT INTO users (name,email,phone_number,password)
+VALUES
+('seb','seb@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('jess','jess@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('robin','robin@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('dom','dom@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('francis','francis@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('hans','hans@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('nelly','nelly@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('duncan','duncan@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('olivier','olivier@test.com','514-555-5555','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
