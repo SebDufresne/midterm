d924fb93e649974d5481996a9cb887c901e849a0
robinwebber <robinewebber@gmail.com>
2019-07-26 20:24:44 +0000

    dog_menu table created

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -1,2 +1,12 @@
 DROP TABLE IF EXISTS orders CASCADE;
 
+CREATE TYPE orderstatus AS ENUM ('fulfilled', 'active', 'declined')
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+  ordered_at TIMESTAMP,
+  status ORDERSTATUS DEFAULT NULL,
+  total_cost INTEGER DEFAULT NULL,
+  comments TEXT
+);
