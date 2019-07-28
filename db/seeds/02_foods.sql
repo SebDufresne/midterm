6af5ea0b09ad4ef30064a17e7eb23a02ee5eab94
Jess <jnoriegaless@gmail.com>
2019-07-28 01:12:33 +0000

    Add a new image URL to display in index.ejs file

--- db/seeds/02_foods.sql
+++ db/seeds/02_foods.sql
@@ -2,12 +2,12 @@
 
 INSERT INTO foods (name,description,price,picture_url,vegan,vegetarian,gluten_free)
 VALUES
-('Tubby Dog', 'homemade chili, bacon, cheese, onions, mustard',799,'',TRUE,TRUE,FALSE),
+('Tubby Dog', 'homemade chili, bacon, cheese, onions, mustard',799,'src/images/foods/dog_tubbydog.jpg',TRUE,TRUE,FALSE),
 ('Cheetah', 'sauerkraut, cheese, mustard, relish',899,'src/images/foods/dog_cheetah.jpg',FALSE,FALSE,TRUE),
-('Yogi Bear', 'mustard, ketchup, relish, onions, cheese slices',799,'',TRUE,TRUE,FALSE),
-('Harlo', 'bacon, cheese, mustard, relish',799,'',FALSE,FALSE,FALSE),
+('Yogi Bear', 'mustard, ketchup, relish, onions, cheese slices',799,'src/images/foods/dog_yogi.jpg',TRUE,TRUE,FALSE),
+('Harlo', 'bacon, cheese, mustard, relish',799,'src/images/foods/dog_harlo.jpg',FALSE,FALSE,FALSE),
 ('A-Bomb', 'cheese, bacon, mayo, mustard, ketchup, potato chips',799,'src/images/foods/dog_abomb.jpg',TRUE,FALSE,FALSE),
 ('Sumo', 'Japanese mayo, wasabi, pickled ginger, lightly toasted sesame seeds, seaweed salad',799,'src/images/foods/dog_sumo.jpg',FALSE,FALSE,FALSE),
-('Slaw Dog', 'coleslaw, jpmemade chili, mustard, cheese, pickles',799,'',FALSE,FALSE,FALSE),
+('Slaw Dog', 'coleslaw, jpmemade chili, mustard, cheese, pickles',799,'src/images/foods/dog_slawdog.jpg',FALSE,FALSE,FALSE),
 ('PBJ', 'peanut butter & jelly',799,'src/images/foods/dog_pbj.jpg',FALSE,TRUE,FALSE),
 ('Sherm''s Ultimate Gripper', 'dog is bacon wrapped then deep-fried! topped with ham, homemade chili, cheese, mustard, bacon, hot peppers, onions, and a friend egg',799,'src/images/foods/dog_gripper.jpg',FALSE,FALSE,FALSE);
--- public/scripts/event_handler.js
+++ public/scripts/event_handler.js
@@ -1,3 +1,3 @@
-$( "#target" ).click(function() {
+$( "#add-item" ).click(function() {
   alert( "Handler for .click() called." );
 });
