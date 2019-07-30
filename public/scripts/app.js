dac65e2e5682f2d5b3cf367beef55e9097a5de31
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-30 06:39:44 +0000

    Added counters for total price of order

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,7 @@ $(() => {
   $(".add-food-btn").click(function() {
     const foodId = $(this).val();
 
+    let priceStr = $(this).closest("div").find(".price").text();
     const $cartField = $("input[name='cart']");
 
     const cartStr = $cartField.val();
@@ -24,6 +25,34 @@ $(() => {
 
     console.log('newCartStr', newCartStr); // Seb
 
+
+    // Price is like 0.00$, let's change it to a number:
+    priceStr = priceStr.replace(/[.$]/g, '');
+    const price = Number(priceStr);
+
+    console.log('price', priceStr); // Seb
+
+
     $cartField.val(newCartStr);
+
+    const $headerField = $("nav>h1");
+
+    let headerValue = $headerField.text();
+
+    headerValue = headerValue.replace(/[.$]/g, '');
+
+    if (Number(headerValue)) { // If it's not a number, will return NaN which is false
+      headerValue = Number(headerValue);
+      headerValue += price;
+    } else {
+      headerValue = price;
+    }
+
+    headerValue = (headerValue / 100).toFixed(2) + '$';
+
+
+
+    $headerField.text(headerValue);
+
   });
 });
