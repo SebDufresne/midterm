eeaeba88e561b18915c312a4ec70da487d1d6f61
Jess <jnoriegaless@gmail.com>
2019-07-31 21:47:11 +0000

    Ensure INSERT is in DB. Make changes to get route of orders.js. Send params to new_order.ejs page

--- routes/orders.js
+++ routes/orders.js
@@ -5,64 +5,63 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
+const { sendSMS } = require("../public/scripts/sms");
 
-const { sendSMS } = require('../public/scripts/sms');
-
-const { getUserInfo, getPhoneNumber, refactorOrder } = require('../lib/helpers');
+const {
+  getUserInfo,
+  getPhoneNumber,
+  refactorOrder
+} = require("../lib/helpers");
 
 module.exports = (db, iconsKey) => {
   router.get("/", (req, res) => {
-    const userId = req.session.userId || '';
+    const userId = req.session.userId || "";
 
     if (userId) {
       getUserInfo(userId, db)
         .then(userInfo => {
-
           if (!userInfo.admin) {
-
             const getOrdersQuery = {
               text: `SELECT * FROM order_summary WHERE user_id = $1`,
-              values: [userId],
+              values: [userId]
             };
 
             db.query(getOrdersQuery)
               .then(data => {
-                const orderData = data.rows;
+                const currentOrder = data.rows;
 
-                const structuredOrders = refactorOrder(orderData);
+                const structuredOrders = refactorOrder(currentOrder);
 
                 const user = userInfo;
-                const params = {user, structuredOrders, iconsKey};
+                const params = { user, structuredOrders, iconsKey };
 
                 console.log(structuredOrders);
                 res.render("orders", params);
               })
               .catch(err => {
-                res
-                  .status(500)
-                  .json({ error: err.message });
+                res.status(500).json({ error: err.message });
               });
           } else {
             const user = userInfo;
-            const params = {user, iconsKey};
+            const params = { user, iconsKey };
             res.render("404", params);
           }
         })
         .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
+          res.status(500).json({ error: err.message });
         });
     } else {
-      res.redirect('/');
+      res.redirect("/");
     }
   });
 
+  let orderId = "";
+
   router.post("/", (req, res) => {
-    const userId = req.session.userId || '';
+    const userId = req.session.userId || "";
     const processedOrder = req.body.cart;
     const cart = JSON.parse(processedOrder);
 
@@ -77,65 +76,88 @@ module.exports = (db, iconsKey) => {
         }
 
         const insertOrders = {
-          text: 'INSERT INTO orders (user_id, total_cost) VALUES ($1, $2) RETURNING id',
-          values: [userId, totalCost],
-        }
+          text:
+            "INSERT INTO orders (user_id, total_cost) VALUES ($1, $2) RETURNING id",
+          values: [userId, totalCost]
+        };
 
         console.log(">>>insertOrders<<<", insertOrders);
 
         db.query(insertOrders)
-        .then(data => {
-          orderId = data.rows[0].id;
+          .then(data => {
+            orderId = data.rows[0].id;
 
-          for (let item of cart) {
-            const {id, name, price, qty} = item;
+            for (let item of cart) {
+              const { id, name, price, qty } = item;
 
-            for (let i = 0; i < qty; i++) {
-              const insertFoodOrders = {
-                text: 'INSERT INTO food_orders (order_id, food_id) VALUES ($1, $2)',
-                values: [orderId, item.id],
+              for (let i = 0; i < qty; i++) {
+                const insertFoodOrders = {
+                  text: "INSERT INTO food_orders (order_id, food_id) VALUES ($1, $2)",
+                  values: [orderId, item.id]
+                };
+
+                db.query(insertFoodOrders)
+                  .then(data => {
+                    console.log(
+                      ">>>>>>>>>insertFoodOrders<<<<<<<<<",
+                      insertFoodOrders
+                    );
+                  })
+                  .catch(err => {
+                    res.status(500).json({ error: err.message });
+                  });
               }
-              console.log(">>>>>>>>>insertFoodOrders<<<<<<<<<", insertFoodOrders);
             }
-          }
 
-          const ownerId = 3;
-          sendSMS(getPhoneNumber(ownerId), `A new 🌭 order has been placed. The order number is ${orderId}.`);
+            const ownerId = 3;
+            sendSMS(
+              getPhoneNumber(ownerId),
+              `A new 🌭 order has been placed. The order number is ${orderId}.`
+            );
 
-          req.session.cart = null;
-          const params = {user, cart, iconsKey};
-          res.redirect('/orders/:id');
-
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-
+            req.session.cart = null;
+            const params = { user, cart, iconsKey };
+            res.redirect(`/orders/${orderId}`);
+          })
+          .catch(err => {
+            res.status(500).json({ error: err.message });
+          });
       })
       .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.get("/:id", (req, res) => {
-    const userId = req.session.userId || '';
+  router.get('/:id', (req, res) => {
+    const userId = req.session.userId || "";
+    const orderIdForUser = req.params.id;
 
-    if (userId) {
-      const user = userInfo;
-      const params = {user, iconsKey};
-      res.render("/", params);
-    }
+    getUserInfo(userId, db)
+      .then(userInfo => {
+        if (userId) {
+          const orderSummQuery = `SELECT * FROM order_summary WHERE order_id = ${orderIdForUser}`;
 
-    if (!userId) {
-      res.redirect('/login');
-    }
+          db.query(orderSummQuery)
+            .then(data => {
+              const orderData = data.rows;
+              const structuredOrders = refactorOrder(orderData)[0];
+              const user = userInfo;
 
+              const params = { user, structuredOrders, iconsKey };
+              res.render("new_order", params);
+            })
+            .catch(err => {
+              res.status(500).json({ error: err.message });
+            });
+        } else {
+          const user = userInfo;
+          const params = { user, iconsKey };
+          res.render("404", params);
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
   });
-
   return router;
-
 };
