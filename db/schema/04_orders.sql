85e9dceb61fcbfb9c541c9167f4437530511af9d
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-31 15:33:05 +0000

    Updated SQL schema for default timestanmp

--- db/schema/04_orders.sql
+++ db/schema/04_orders.sql
@@ -6,7 +6,7 @@ CREATE TYPE status_opt AS ENUM('new', 'processing', 'declined', 'fulfilled');
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  ordered_at TIMESTAMP,
+  ordered_at TIMESTAMP DEFAULT NOW(),
   status STATUS_OPT DEFAULT 'new',
   total_cost INTEGER DEFAULT NULL,
   customer_comments TEXT
